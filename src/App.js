966e2b66e9d03d26c8f8d64a08dde4d591eb1cc9
RonanKhalilOlaes <113484040+RonanKhalilOlaes@users.noreply.github.com>
2023-09-19 01:03:25 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <p>this is a test for Github</p>
+    </div>
+  );
+}
